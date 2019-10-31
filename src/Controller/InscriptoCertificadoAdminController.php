1d9e7df6dbd83100222260928b2e8b99db155f1c
Jonathan Ramirez <jonny_0385@hotmail.com>
2019-10-31 17:42:16 -0300

    ultimas correcciones para finalizar la v1.0

--- src/Controller/InscriptoCertificadoAdminController.php
+++ src/Controller/InscriptoCertificadoAdminController.php
@@ -51,7 +51,7 @@ final class InscriptoCertificadoAdminController extends CRUDController
             // set bacground image
             for ($i = 1; $i <= $pdf->getNumPages(); $i++) {
                 $pdf->setPage($i);
-                $img_file = $this->getParameter('kernel.project_dir') . '/public/images/imagencertificado.jpg';
+                $img_file = $this->getParameter('kernel.project_dir') . '/public/bundles/images/imagencertificado.png';
                 $pdf->Image($img_file, 0, 0, 300, 210, '', '', '', false, 300, '', false, false, 0);
             }
             // restore auto-page-break status
